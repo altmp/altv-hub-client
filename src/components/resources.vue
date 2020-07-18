3f5a3561106368589af432e62731d62d701ae598
kubeeek <jakub.galecki5@gmail.com>
2020-07-18 15:53:22 +0200

    fixed console errors

--- src/components/resources.vue
+++ src/components/resources.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="resources" v-if="resources.length >= 1">
+    <div class="resources" v-if="paginatedResources.length >= 1">
         <resource-card
             v-for="resource in paginatedResources"
             :key="resource.url"
             :resource="resource"
         />
     </div>
-    <div v-else class="lds-heart">
-        <div></div>
+    <div v-else>
+        <p>There are no resources like that.</p>
     </div>
 </template>
 
@@ -70,6 +70,8 @@ export default {
             return sortedResources;
         },
         paginatedResources() {
+            if (this.formattedResources.length === 0) return [];
+
             let chunkedResources = this.array_chunk(this.formattedResources, this.perPage);
             let resources = [];
 
